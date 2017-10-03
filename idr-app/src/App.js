607155a955bc2a19f8c99c9786dafdd03e53727e
minhvvu <vn.vietvu@gmail.com>
2017-10-03 23:52:45 +0200

    Add test data, follow a tut

--- idr-app/src/App.js
+++ idr-app/src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import data from './test_data.js';
+import Chart from './components/chart';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        IDR Interactive Dimensionality Reduction
+        <p>IDR Interactive Dimensionality Reduction</p>
+        <Chart data={data} />
       </div>
     );
   }
