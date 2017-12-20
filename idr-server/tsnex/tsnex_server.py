af2ac62cc5a952aa98f60380aa97ab88b91ee6bf
minhvvu <vn.vietvu@gmail.com>
2017-12-20 17:09:38 +0100

    Test flask socketio

--- idr-server/tsnex/tsnex_server.py
+++ idr-server/tsnex/tsnex_server.py
@@ -1,3 +1,16 @@
 # tsnex_server.py
 # minh, 20/12/2017
-print("TSNEX_SERVER")
+
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECREST_KEY'] = 'tsnex';
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return "Hello from flask-socket-io"
+
+if __name__ == '__main__':
+    socketio.run(app, host='127.0.0.1', port=5000, debug=True)
