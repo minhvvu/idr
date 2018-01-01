ec1e3c7c044cb76552a71e28982292ebcd2b8824
minhvvu <vn.vietvu@gmail.com>
2018-01-01 22:55:15 +0100

    Refactor and document Commands.elm

--- idr-elm/src/Commands.elm
+++ idr-elm/src/Commands.elm
@@ -8,41 +8,57 @@ import Common exposing (Point)
 import Msgs exposing (Msg)
 
 
+{-| Socket server URI
+-}
 socketServer : String
 socketServer =
     "ws://127.0.0.1:5000/tsnex"
 
 
+{-| Socket endpoint for transforming data from server to client
+-}
 getDataURI : String
 getDataURI =
     socketServer ++ "/get_data"
 
 
+{-| Socket endpoint for transforming list of moved points from client to server
+-}
 movedPointsURI : String
 movedPointsURI =
     socketServer ++ "/moved_points"
 
 
+{-| Client subscription to listen to the new data from server
+-}
 listenToNewData : Sub Msg
 listenToNewData =
     WebSocket.listen getDataURI Msgs.NewData
 
 
+{-| Client command to request the initial data
+-}
 getInitData : Cmd Msg
 getInitData =
     WebSocket.send getDataURI "Get Initial Data"
 
 
+{-| Client command to request new data
+-}
 getNewData : Cmd Msg
 getNewData =
     WebSocket.send getDataURI "Request data from client"
 
 
+{-| Client command to send a list of Points to server
+-}
 sendMovedPoints : List Point -> Cmd Msg
 sendMovedPoints points =
-    WebSocket.send movedPointsURI (Encode.encode 4 (encodeListPoints points))
+    WebSocket.send movedPointsURI (encodeListPoints points)
 
 
+{-| Util function to describe how to deocde json to a Point object
+-}
 pointDecoder : Decode.Decoder Point
 pointDecoder =
     decode Point
@@ -51,18 +67,10 @@ pointDecoder =
         |> required "y" Decode.float
 
 
-listPointsDecoder : Decode.Decoder (List Point)
-listPointsDecoder =
-    Decode.list pointDecoder
-
-
-decodeListPoints : String -> Result String (List Point)
-decodeListPoints str =
-    Decode.decodeString listPointsDecoder str
-
-
-encodePoint : Point -> Encode.Value
-encodePoint point =
+{-| Util function to describle how to encode a Point object to json
+-}
+pointEncoder : Point -> Encode.Value
+pointEncoder point =
     Encode.object
         [ ( "id", Encode.string point.id )
         , ( "x", Encode.float point.x )
@@ -70,6 +78,37 @@ encodePoint point =
         ]
 
 
-encodeListPoints : List Point -> Encode.Value
+{-| Util function to describle how to decode json to a list of Point objects
+-}
+listPointsDecoder : Decode.Decoder (List Point)
+listPointsDecoder =
+    Decode.list pointDecoder
+
+
+{-| Util function to describle how to encode a list of Point objects to json
+-}
+listPointEncoder : List Point -> Encode.Value
+listPointEncoder points =
+    Encode.list (List.map pointEncoder points)
+
+
+{-| Util function to decode a json to a list of Point object
+-}
+decodeListPoints : String -> Result String (List Point)
+decodeListPoints str =
+    Decode.decodeString listPointsDecoder str
+
+
+{-| Util function to encode a list of Point objects into json
+-}
+encodeListPoints : List Point -> String
 encodeListPoints points =
-    Encode.list (List.map encodePoint points)
+    let
+        pretyPrint =
+            1
+
+        {--0 for disable prety json string --}
+    in
+        points
+            |> listPointEncoder
+            |> Encode.encode pretyPrint
