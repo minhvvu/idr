84b55d6d6f5df8e2c8de88403e2a02b755a5e36c
minhvvu <vn.vietvu@gmail.com>
2018-01-25 10:56:18 +0100

    Delete old client angular app

--- idr-app/src/data/IDRActionTypes.js
+++
@@ -1,9 +0,0 @@
-//IDRActionTypes
-
-const ActionTypes = {
-  ADD_TODO: 'ADD_TODO',
-  REFRESH_DATASET: 'REFRESH_DATASET',
-  MOVE_POINT: 'MOVE_POINT',
-};
-
-export default ActionTypes;
--- idr-app/src/data/IDRDispatcher.js
+++
@@ -1,6 +0,0 @@
-//IDRDispacher.js
-
-import { Dispatcher } from 'flux';
-
-// in an app, there is only one dispatcher (singleton)
-export default new Dispatcher();
