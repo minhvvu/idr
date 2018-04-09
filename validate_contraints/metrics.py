48740170cbf428f9b2821feefece0eb92f1d9e18
minhvvu <vn.vietvu@gmail.com>
2018-04-09 12:01:15 +0200

    Fix CCA, speed up logRNX

--- validate_contraints/metrics.py
+++ validate_contraints/metrics.py
@@ -2,13 +2,17 @@
 
 import math
 import numpy as np
+from numpy.linalg import norm
 from scipy.spatial.distance import pdist, squareform
 from scipy.stats import pearsonr
 from sklearn.preprocessing import scale
+from sklearn.isotonic import IsotonicRegression
+
+MACHINE_EPSILON = np.finfo(np.double).eps
 
 
 class DRMetric(object):
-    """ Metric measurement for DR methods
+    """ Metric measurements for DR methods
     """
 
     def __init__(self, X, Y):
@@ -16,27 +20,26 @@ class DRMetric(object):
         Args:
             X (ndarray): input data in high dimensional space
             Y (ndarray): embedded result in low dimensional space
-
         """
         super(DRMetric, self).__init__()
-        # data shape
         self.n_samples = X.shape[0]
-        self.n_high_dim = X.shape[1]
-        self.n_low_dim = Y.shape[1]
+        # self.n_high_dim = X.shape[1]
+        # self.n_low_dim = Y.shape[1]
 
-        # store high-dim and low-dim data
         self.X = X
         self.Y = Y
 
         # pre-calculate pairwise distance in high-dim and low-dim
-        self.pdX = pdist(self.X, "euclidean")
-        self.pdY = pdist(self.Y, "euclidean")
-        self.dX = squareform(pdist(self.X, "sqeuclidean"))
-        self.dY = squareform(pdist(self.Y, "sqeuclidean"))
+        self.dX = pdist(X, "euclidean")
+        self.dY = pdist(Y, "euclidean")
+        # self.dX = np.maximum(self.dX, MACHINE_EPSILON)
+        # self.dY = np.maximum(self.dY, MACHINE_EPSILON)
 
         # index of all neighbors with ascending distances
-        self.idX = np.argsort(self.dX, axis=1)[:, 1:]
-        self.idY = np.argsort(self.dY, axis=1)[:, 1:]
+        dXSquare = squareform(self.dX**2)
+        dYSquare = squareform(self.dY**2)
+        self.idX = np.argsort(dXSquare, axis=1)[:, 1:]
+        self.idY = np.argsort(dYSquare, axis=1)[:, 1:]
 
     def _Qnx(self, k):
         """Calculate $Q_{NX}(k)= \\
@@ -50,7 +53,8 @@ class DRMetric(object):
 
         Vk = self.idX[:, :k]
         Nk = self.idY[:, :k]
-        q_nx = sum([len(set(a) & set(b)) for a, b in zip(Vk, Nk)])
+        q_nx = sum([np.intersect1d(a, b, assume_unique=True).size
+                    for a, b in zip(Vk, Nk)])
         q_nx /= (k * self.n_samples)
 
         assert 0.0 <= q_nx <= 1.0
@@ -73,11 +77,62 @@ class DRMetric(object):
         """Calculate Area under the $R_{NX}(k)$ curve in the log-scale of $k$
         """
         auc = sum([self._Rnx(k) / k for k in range(1, self.n_samples - 1)])
-        norm = sum([1 / k for k in range(1, self.n_samples - 1)])
-        auc /= norm
+        norm_const = sum([1 / k for k in range(1, self.n_samples - 1)])
+        auc /= norm_const
         assert 0.0 <= auc <= 1.0
         return auc
 
     def pearsonr(self):
-        p, _ = pearsonr(self.pdX, self.pdY)
+        """Calculate Pearson correlation coefficient b.w. two vectors
+        """
+        p, _ = pearsonr(self.dX, self.dY)
         return p
+
+    def cca_stress(self):
+        """Curvilinear Component Analysis Stress function
+            $$ S = \sum_{ij}^{N}
+                (d^{x}_{ij} - d^{y}_{ij})^2 F_{\lambda}(d^{y}_{ij})
+            $$
+            where $d^{x}_{ij}$ is pairwise distance in high-dim,
+            $d^{y}_{ij}$ is pairwise distance in low-dim,
+            $F_{\lambda}(d^{*}_{ij}$ is decreasing weighting-function.
+            For CCA, there are some choises for weighting-function:
+            e.g. step function (depends $\lambda$), exponential func or
+            $F(d^{y}_{ij}) = 1 - sigmoid(d^{y}_{ij}$.
+        """
+        dX = scale(self.dX)
+        dY = scale(self.dY)
+        diff = dX - dY
+        weight = 1.0 - 1.0 / (1.0 + np.exp(-dY))
+        stress = np.dot(diff**2, weight)
+        return stress
+
+    def mds_isotonic(self):
+        """Stress function of MDS
+            + Pairwise distances vector in high-dim is fitted into an
+            Isotonic Regression model
+            + The stressMDS function is then applied for the isotonic-fitted
+            vector and the pairwise distance vector in low-dim
+            $$ S = \sqrt
+                { \sum_{ij}^{N} (d^{iso}_{ij} - d^{y}_{ij})^2 }
+                { \sum_{ij}^{N} d^{y}_{ij} }
+            $$
+            where $d^{y}_{ij}$ is pairwise distance in low-dim.
+        """
+        dX = scale(self.dX)
+        dY = scale(self.dY)
+        ir = IsotonicRegression()
+        dYh = ir.fit_transform(X=dX, y=dY)
+        return norm(dYh - dY) / norm(dY)
+
+    def sammon_nlm(self):
+        """Stree function for Sammon Nonlinear mapping
+            $ S = \frac{1}{\sum_{ij} d^{x}_{ij}}
+                \sum_{ij} \frac{ (d^{x}_{ij} - d^{y}_{ij})^2 }{d^{x}_{ij}]}
+            $
+        """
+        dX_inv = 1.0 / self.dX
+        dX_inv[np.isinf(dX_inv)] = 0
+        diff = self.dX - self.dY
+        stress = np.dot((diff ** 2), dX_inv)
+        return stress / self.dX.sum()
--- validate_contraints/metrics_test.py
+++ validate_contraints/metrics_test.py
@@ -2,14 +2,15 @@
 
 import math
 import numpy as np
+import time
 import timeit
 
 # import all Adrien's metric function
 from Adrien_metrics.Correlation_Coefficient import Correlation_Coefficient as Corr
+from Adrien_metrics.K_Neighborhood import K_Neighborhood as LogRNX
 from Adrien_metrics.Curvilinear_Component_Analysis import CCA as CCA
-from Adrien_metrics.K_Neighborhood import K_Neighborhood as logRNX
-from Adrien_metrics.Sammon_s_Non_Linear_Mapping import NLM as SOM
-from Adrien_metrics.Stress_Function import Stress as MDSStress
+from Adrien_metrics.Sammon_s_Non_Linear_Mapping import NLM as NLM
+from Adrien_metrics.Stress_Function import Stress as MDS
 
 
 # import Minh's metric function
@@ -22,33 +23,43 @@ from sklearn.manifold import TSNE
 rel_tol = 1e-3
 
 # prepare test dataset
-ds = datasets.load_iris()
-X_hd = ds.data
-target_labels = ds.target
+# ds = datasets.load_iris()
+# X_hd = ds.data
+# target_labels = ds.target
+
+X_hd, target_labels = datasets.samples_generator.make_swiss_roll(
+    n_samples=100, random_state=0) # make_s_curve
+
 
 # run tsne to get embedded result in low dim.
-tsne = TSNE(random_state=None)
+tsne = TSNE(random_state=0)
 print(tsne)
 X_ld = tsne.fit_transform(X_hd)
 
 # create DRMetric object
+tic = time.time()
 drMetric = DRMetric(X_hd, X_ld)
+init_time = time.time() - tic
 
-# test logRNX
-m1 = logRNX.compute(X_hd, X_ld)
-m2 = drMetric.auc_rnx()
-print('Test logRNX', m1, m2)
-# np.testing.assert_almost_equal(m1, m2)
-assert math.isclose(m1, m2, rel_tol=rel_tol, abs_tol=0.0) is True
+test_suites = {
+    'Test logRNX': [LogRNX.compute, drMetric.auc_rnx],
+    'Test CorrCoef': [Corr.compute, drMetric.pearsonr],
+    'Test MDS Stress': [MDS.compute, drMetric.mds_isotonic],
+    'Test CCA Stress': [CCA.compute, drMetric.cca_stress],
+    #'Test Sammon NLM': [NLM.compute, drMetric.sammon_nlm],
+}
 
-# test correlation coefficient
-m1 = Corr.compute(X_hd, X_ld)
-m2 = drMetric.pearsonr()
-print('Test correlation coefficient', m1, m2)
-# np.testing.assert_almost_equal(m1, m2)
-assert math.isclose(m1, m2, rel_tol=rel_tol, abs_tol=0.0) is True
+print("Start test suite")
+for test_name, funcs in test_suites.items():
+    print(test_name)
+    tic = time.time()
+    m1 = funcs[0](X_hd, X_ld)
+    run1 = time.time() - tic
+    print("m1 = {:10.4f} ({:10.4f} s)".format(m1, run1))
 
-# def f1(): return Corr.compute(X_hd, X_ld)
-# t1 = timeit.timeit(f1, number=20)
-# t2 = timeit.timeit(drMetric.pearsonr, number=20)
-# print(t1, t2)
+    tic = time.time()
+    m2 = funcs[1]()
+    run2 = time.time() - tic
+    print("m2 = {:10.4f} ({:10.4f} s)".format(m2, run2))
+
+    assert math.isclose(m1, m2, rel_tol=rel_tol, abs_tol=0.0) is True
